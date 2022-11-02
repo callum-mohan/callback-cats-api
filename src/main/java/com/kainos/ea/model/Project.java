aa614ed24022a50925490d4ccda95a8cc31bd2f3
NiamhCarville <niamh.carville@kainos.com>
2022-11-02 13:29:25 +0000

    added rest of models

---
+++ src/main/java/com/kainos/ea/model/Project.java
@@ -0,0 +1,37 @@
+package com.kainos.ea.model;
+
+public class Project {
+    private int projectId;
+    private String projectName;
+    private String projectDescription;
+
+    public Project(int projectId, String projectName, String projectDescription) {
+      setProjectId(projectId);
+      setProjectName(projectName);
+      setProjectDescription(projectDescription);
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }
+}
--- src/main/java/com/kainos/ea/model/SalesEmployee.java
+++ src/main/java/com/kainos/ea/model/SalesEmployee.java
@@ -4,7 +4,7 @@ public class SalesEmployee extends Employee{
 
     private float commissionRate;
     private float totalSales;
-    
+
     public float getCommissionRate() {
         return commissionRate;
     }
@@ -17,8 +17,7 @@ public class SalesEmployee extends Employee{
     public void setTotalSales(float totalSales) {
         this.totalSales = totalSales;
     }
-    
-    
+
     public SalesEmployee(int employeeId, String first_name, String last_name, String address, String postcode, String nin, String bankNo, float startSalary, int departmentId, float commissionRate, float totalSales) {
         super(employeeId, first_name, last_name, address, postcode, nin, bankNo, startSalary, departmentId);
         setCommissionRate(commissionRate);
